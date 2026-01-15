0e580951b80b8bcc4bc631f7fb47b74ed1113507
godthunder <1040279302@qq.com>
2026-01-15 14:37:12 +0800

    注册和登录模块（中）

---
+++ web/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <NavBar />
+  <router-view></router-view>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+//import $ from 'jquery'
+
+export default {
+  components: {
+    NavBar
+  },
+  setup(){
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: 'zx',
+    //     password: 'zx'
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxNzNmZGZiNGRiN2I0ODQxYjQ3ZDk0MGRmMDNiZWEwYiIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTc2ODM3NjQwNSwiZXhwIjoxNzY5NTg2MDA1fQ.XnPpazbvV0LbahtqlxeBhvKP8VR4pcP4R9kXrKIzBag"
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: 'zx1',
+    //     password: '123',
+    //     confirmedPassword: '123'
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   },
+    // })
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url("@/assets/images/background.png");
+  background-size: cover;
+}
+</style>
---
+++ web/src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
+import NotFound from '../views/error/NotFound'
+import UserAccountLoginView from '../views/user/account/UserAccountLoginView'
+import UserAccountRegisterView from '../views/user/account/UserAccountRegisterView'
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+  path: "/user/account/login/",
+  name: "user_account_login",
+  component: UserAccountLoginView,
+},
+  {
+  path: "/user/account/register/",
+  name: "user_account_register",
+  component: UserAccountRegisterView,
+},
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
