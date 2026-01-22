10de06d268ce2a2a50f94e2f3b0cddee6fd35a40
godthunder <1040279302@qq.com>
2026-01-23 00:26:01 +0800

    创建个人页面（上）

---
+++ backend/src/main/java/com/kob/backend/service/user/bot/GetListService.java
@@ -0,0 +1,9 @@
+package com.kob.backend.service.user.bot;
+
+import com.kob.backend.pojo.Bot;
+
+import java.util.List;
+
+public interface GetListService {
+    List<Bot> getList();//信息穿在token里面，可以不需要带参数
+}
--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,10 +6,79 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex'
 
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "POST",
+        //     data: {
+        //         title: "Bot的标题",
+        //         description: "Bot的描述",
+        //         content: "Bot的代码",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     }, error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/remove/",
+        //     type: "POST",
+        //     data: {
+        //         bot_id: 9,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     }, error(resp) {
+        //         console.log(resp);
+        //     }
+        // }),
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/update/",
+        //     type: "POST",
+        //     data: {
+        //         bot_id: 8,
+        //         title: "更新后的Bot标题",
+        //         description: "更新后的Bot描述",
+        //         content: "更新后的Bot代码",
+
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     }, error(resp) {
+        //         console.log(resp);
+        //     }
+        // }),
+        $.ajax({
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp){
+                console.log(resp);
+            }, error(resp) {
+                console.log(resp);
+            }
+        })
+
     }
 }
 </script>
